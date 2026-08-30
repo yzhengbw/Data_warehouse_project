b5502fb1d37afbc6ea137e753af150d0bf45e28a
Hazelll <yzhengbw@outlook.com>
2026-08-30 22:26:18 +1000

    adjust folder structure and add explanations for the scripts

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -1,5 +1,13 @@
-USE DataWarehouse;
-GO
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema, dropping existing tables 
+    if they already exist.
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
 
 IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
     DROP TABLE bronze.crm_cust_info;
