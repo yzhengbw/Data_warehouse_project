00c9f83968c0dbb08e8646dc387b5c045d22873c
Hazelll <yzhengbw@outlook.com>
2026-08-31 13:21:21 +1000

    create ddl for silver layer

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,8 +1,8 @@
-IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
-    DROP TABLE bronze.crm_cust_info;
+IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_cust_info;
 GO
 
-CREATE TABLE bronze.crm_cust_info(
+CREATE TABLE silver.crm_cust_info(
     cst_id INT,
     cst_key NVARCHAR(50),
     cst_firstname NVARCHAR(50),
@@ -13,11 +13,11 @@ CREATE TABLE bronze.crm_cust_info(
 );
 GO
 
-IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
-    DROP TABLE bronze.crm_prd_info;
+IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_prd_info;
 GO
 
-CREATE TABLE bronze.crm_prd_info(
+CREATE TABLE silver.crm_prd_info(
     prd_id INT,
     prd_key NVARCHAR(50),
     prd_nm NVARCHAR(50),
@@ -28,11 +28,11 @@ CREATE TABLE bronze.crm_prd_info(
 )
 GO
 
-IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
-    DROP TABLE bronze.crm_sales_details;
+IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE silver.crm_sales_details;
 GO
 
-CREATE TABLE bronze.crm_sales_details (
+CREATE TABLE silver.crm_sales_details (
     sls_ord_num NVARCHAR(50),
     sls_prd_key NVARCHAR(50),
     sls_cust_id INT,
@@ -45,32 +45,32 @@ CREATE TABLE bronze.crm_sales_details (
 );
 GO
 
-IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
-    DROP TABLE bronze.erp_loc_a101;
+IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE silver.erp_loc_a101;
 GO
 
-CREATE TABLE bronze.erp_loc_a101 (
+CREATE TABLE silver.erp_loc_a101 (
     cid NVARCHAR(50),
     cntry NVARCHAR(50)
 );
 GO
 
-IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
-    DROP TABLE bronze.erp_cust_az12;
+IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE silver.erp_cust_az12;
 GO
 
-CREATE TABLE bronze.erp_cust_az12 (
+CREATE TABLE silver.erp_cust_az12 (
     cid NVARCHAR(50),
     bdate DATE,
     gen NVARCHAR(50)
 );
 GO
 
-IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
-    DROP TABLE bronze.erp_px_cat_g1v2;
+IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE silver.erp_px_cat_g1v2;
 GO
 
-CREATE TABLE bronze.erp_px_cat_g1v2 (
+CREATE TABLE silver.erp_px_cat_g1v2 (
     id NVARCHAR(50),
     cat NVARCHAR(50),
     subcat NVARCHAR(50),
